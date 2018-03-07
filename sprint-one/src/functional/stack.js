df52d552aa5c20e140e0865a7c166e29ad88410b
Fred Zirdung <fred@hackreactor.com>
2018-03-07 11:06:19 -0800

    Pass the first 3 tests.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,19 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    counter++;
   };
 
   someInstance.pop = function() {
+    counter--;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
