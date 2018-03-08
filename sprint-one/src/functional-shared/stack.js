93c737cf15a91a469b3e94d2b9b7b130d078015a
Rongsiu Lim <rongsiu@Rongsius-MacBook-Pro.local>
2018-03-07 19:06:48 -0800

    complete functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
+var counter;
+var storage;
+
 var Stack = function() {
+  var someInstance = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  storage = {}
+  counter = 0
+  extend(someInstance, stackMethods)
+  return someInstance
 };
 
-var stackMethods = {};
+var extend = function(someInstance, stackMethods) {
+  for(var key in stackMethods) {
+    someInstance[key] = stackMethods[key]
+  }
+};
+
+var stackMethods = {
+  push: function(value) {
+    storage[counter] = value;
+    counter++;
+  },
+  pop: function() {
+    counter--;
+    return storage[counter]
+  },
+  size: function() {
+  	return Math.max(0,counter);
+  }
+};
+
+
+
 
 
