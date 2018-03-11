66afe98f83649d29c73269fabd76ad130a8b6278
shmily40686 <shmily40686@gmail.com>
2018-03-10 16:49:51 -0800

    complete data structure sprint

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,43 +6,51 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-
-  // var callback = function(item, key,storage) {
-  //   if(key === index) {
-  //       // set this value to tube have same index
-  //   }
-  // }
-  //   this._storage.each(callback)
-    this._storage.set(index, v);
-//   }
-// }
-
-
-// if(storage[index] === undefined) {
-//   this._storage.set(index, v);
-// } else {
-//   var bucket = []
-//     bucket[0] = k;
-//     bucket[1] = v;
-// }
-
-
-
-
+var index = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(index) === undefined) {
+    var bucket = [[k,v]];
+    this._storage.set(index, bucket);
+  } else {
+    var holder = this._storage.get(index)
+    for(var i = 0; i < holder.length; i++) {
+      if(holder[i][0] === k) {
+        holder[i][1] = v;
+      } 
+    }
+    this._storage.get(index).push([k,v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-   return this._storage.get(index);
+  var bucket = this._storage.get(index);
+
+  if(bucket === undefined) {
+    return undefined
+  }
+
+  for(var i = 0; i < bucket.length; i++) {
+    if(bucket[i][0] === k) {
+      return bucket[i][1]
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
 
-  var callback = function(item, key,storage) {
-    if(key === index) {
-        storage[key] = undefined;
+  if(bucket === undefined) {
+    return;
+  }
+
+  var callback = function(buck, storageindex) {
+    if(storageindex === index) {
+      for(var i = 0; i < buck.length; i++) {
+        if(buck[i][0] === k) {
+          buck[i][0] = null 
+        }
+      }
     }
   }
      this._storage.each(callback)
